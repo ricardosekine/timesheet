3f73824c039305f8e609e36d93fb6ab696aa3631
Ricardo Vilas Boas Sekine <t1306rcr@prestadorcbmp.com.br>
2017-10-11 08:49:46 -0300

    [RVBS] - Estrutura de classes padrão p3

--- timesheet-web/src/main/java/com/stefanini/timesheet/TestBean.java
+++ timesheet-web/src/main/java/com/stefanini/timesheet/TestBean.java
@@ -1,25 +1,35 @@
 package com.stefanini.timesheet;
 
-import javax.faces.bean.ManagedBean;
-import javax.faces.bean.SessionScoped;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
-@ManagedBean
-@SessionScoped
+@Scope("session")
 public class TestBean {
 	
 	@Autowired
 	TestService testService;
+	private String hello;
+	
+	public String printMsgFromSpring() {
+		return "Hello World - Spring + JSF 2!!!";
+	}
+	
+	public void callNewMessage() {
+		this.hello = this.testService.getMessage();
+	}
 	
 	public void setTestService(TestService testService) {
 		this.testService = testService;
 	}
 	
-	public String printMsgFromSpring() {
-		return testService.getMessage();
+	public String getHello() {
+		return hello;
+	}
+
+	public void setHello(String hello) {
+		this.hello = hello;
 	}
 
 }
